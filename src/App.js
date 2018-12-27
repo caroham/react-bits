affe8676c5e30ddde733469a3d7fe0cb40ba2bdb
Carolyn Hampe <carolyn.hampe@gmail.com>
2018-12-26 19:48:28 -0800

    small hello test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "hi!",
+    }
+
+    this.changeMessage = this.changeMessage.bind(this);
+  }
+
+  changeMessage() {
+    let message = (this.state.message === "hi!") ? "hello" : "hi!";
+    this.setState({message});
+  } 
+
   render() {
     return (
       <div className="App">
-        <h1>Hi</h1>
+        <h1>{this.state.message}</h1>
+        <button onClick={this.changeMessage}>Click me!</button>
       </div>
     );
   }
